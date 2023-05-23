30bb1458b695eb22f14228b019242c9a8376c2a2
Lidiya135 <lidiyawati315@gmail.com>
2023-05-23 15:15:14 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/greeting', function () {
+    return "Hello World";
+});
+Route::get('/greeting/{id}', [StudentController::class,'show']);
+
+// Route::get('/greeting/{name}', function ($name) {
+//     return "Hello ".$name;
+// })->name('greeting_with_name');
+// Route::get('greeting/{name}', function ($name) {
+//     return view('example', ['name' => $name]);
+// });
